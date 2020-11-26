da0d58eeaab3113a014e3db0f07ca3d8b5353f8d
gabrielvmastery <72418015+gabrielvmastery@users.noreply.github.com>
2020-11-25 23:32:04 -0600

    Update json_parser.py

--- json_parser.py
+++ json_parser.py
@@ -1,10 +1,11 @@
 import json
 import pprint
+from datetime import datetime
 
-with open("edi-210.json", "r") as read_file:
+with open('edi-210.json', 'r') as read_file:
     data = json.load(read_file)
 
-with open("output_file3.json", "w") as write_file:
-    json.dump(data, write_file)
+with open('mastery-json_' + str(datetime.now().strftime('%Y%m%d_%H%M%S')) + '.json', 'w') as write_file:
+    json.dump(data, write_file, indent=2)
     json_string = json.dumps(data, indent=2, sort_keys = False)
     print(json_string)
