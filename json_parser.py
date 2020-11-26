6022af14a669b24e8b9cc25490d94d329f76a18e
gabrielvmastery <72418015+gabrielvmastery@users.noreply.github.com>
2020-11-25 23:25:00 -0600

    Update json_parser.py

--- json_parser.py
+++ json_parser.py
@@ -1,4 +1,10 @@
 import json
+import pprint
 
-with open("data_file.json", "r") as read_file:
+with open("edi-210.json", "r") as read_file:
     data = json.load(read_file)
+
+with open("output_file3.json", "w") as write_file:
+    json.dump(data, write_file)
+    json_string = json.dumps(data, indent=2, sort_keys = False)
+    print(json_string)
