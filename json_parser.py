90e7ee96404c1a87a0fcdc19d1748321102045e9
gabrielvmastery <72418015+gabrielvmastery@users.noreply.github.com>
2020-11-27 13:20:12 -0600

    Update json_parser.py

--- json_parser.py
+++ json_parser.py
@@ -1,11 +1,16 @@
 import json
 import pprint
 from datetime import datetime
+#loadobj= {"president": {"name": "Zaphod Beeblebrox", "species": "Betelgeusian"}}
 
 with open('edi-210.json', 'r') as read_file:
     data = json.load(read_file)
 
-with open('mastery-json_' + str(datetime.now().strftime('%Y%m%d_%H%M%S')) + '.json', 'w') as write_file:
-    json.dump(data, write_file, indent=2)
+with open(str(datetime.now().strftime('%Y%m%d-%H%M%S')) + '-mastery' + '.json', 'w') as write_file:
+
     json_string = json.dumps(data, indent=2, sort_keys = False)
     print(json_string)
+
+    json.dump(data, write_file, indent=2)
+read_file.close()
+write_file.close()
