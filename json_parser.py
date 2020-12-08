fdd920b60970f6a5e5c342835babf65a62755a16
gabrielvmastery <72418015+gabrielvmastery@users.noreply.github.com>
2020-12-08 14:50:09 -0600

    Update json_parser.py

--- json_parser.py
+++ json_parser.py
@@ -1,16 +1,18 @@
 import json
 import pprint
 from datetime import datetime
-#loadobj= {"president": {"name": "Zaphod Beeblebrox", "species": "Betelgeusian"}}
 
-with open('edi-210.json', 'r') as read_file:
+with open('edi-204.json', 'r') as read_file:
     data = json.load(read_file)
 
 with open(str(datetime.now().strftime('%Y%m%d-%H%M%S')) + '-mastery' + '.json', 'w') as write_file:
 
-    json_string = json.dumps(data, indent=2, sort_keys = False)
+    json_string = json.dumps(data, indent=2, separators=(',', ':'), sort_keys = False)
     print(json_string)
 
-    json.dump(data, write_file, indent=2)
+    json.dump(data, write_file, indent=2,separators=(',', ':'))
 read_file.close()
 write_file.close()
+
+#whitespaces are removed after ':' with separators=(',', ':')
+#brackets []{} show up on newlines after ':'
